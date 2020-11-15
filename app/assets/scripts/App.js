0f76d4d2f5b023b08ff9f00a33f3a826d7c33faa
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-11-15 09:27:48 -0600

    Detour into hypotheticals to demonstrate OOP.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,21 @@
 import '../styles/styles.css';
+import Critter from './modules/Critter';
 
 if (module.hot) {
   module.hot.accept();
 }
+
+/* lesson example code below this line */
+// Inherit everything from the parent Critter class
+class Adult extends Critter {
+  payTaxes() {
+    console.log(this.name + ' now owes $0 in taxes!');
+  }
+}
+
+let orson = new Critter('Orson', 'some kinda apple');
+orson.greet();
+
+let manny = new Adult('Manny', 'seaweed');
+manny.greet();
+manny.payTaxes();

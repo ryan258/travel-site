d74b95f2ec7362bc49764f8cb4a20c5cda016209
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-11-16 08:26:35 -0600

    Optimize reveal on scroll with lodash.

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -1,28 +1,53 @@
+import throttle from 'lodash/throttle';
+import debounce from 'lodash/debounce';
+
 class RevealOnScroll {
   constructor() {
     this.itemsToReveal = document.querySelectorAll('.feature-item');
+    this.browserHeight = window.innerHeight;
     this.hideInitially();
+    this.scrollThrottle = throttle(this.calcCaller, 200).bind(this);
     this.events();
   }
 
   events() {
-    window.addEventListener('scroll', () => {
-      this.itemsToReveal.forEach((el) => {
+    window.addEventListener('scroll', this.scrollThrottle);
+    window.addEventListener(
+      'resize',
+      debounce(() => {
+        this.browserHeight = window.innerHeight;
+      }, 300)
+    );
+  }
+
+  calcCaller() {
+    this.itemsToReveal.forEach((el) => {
+      if (el.isRevealed == false) {
         this.calculateIfScrolledTo(el);
-      });
+      }
     });
   }
 
   calculateIfScrolledTo(el) {
-    let scrollPercent =
-      (el.getBoundingClientRect().top / window.innerHeight) * 100;
-    if (scrollPercent < 75) {
-      el.classList.add('reveal-item--is-visible');
+    if (window.scrollY + this.browserHeight > el.offsetTop) {
+      let scrollPercent =
+        (el.getBoundingClientRect().top / this.browserHeight) * 100;
+      if (scrollPercent < 75) {
+        el.classList.add('reveal-item--is-visible');
+        el.isRevealed = true;
+        if (el.isLastItem) {
+          window.removeEventListener('scroll', this.scrollThrottle);
+        }
+      }
     }
   }
 
   hideInitially() {
-    this.itemsToReveal.forEach((el) => el.classList.add('reveal-item'));
+    this.itemsToReveal.forEach((el) => {
+      el.classList.add('reveal-item');
+      el.isRevealed = false;
+    });
+    this.itemsToReveal[this.itemsToReveal.length - 1].isLastItem = true;
   }
 }
 

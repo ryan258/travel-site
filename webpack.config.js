9d68e87a8a7d8b4d663a5ec5efb9e47f927549e5
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-11-19 12:34:22 -0600

    Dynamically inject hashed css and js.

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const currentTask = process.env.npm_lifecycle_event;
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const postcssPlugins = [
   require('postcss-import'),
@@ -31,6 +32,12 @@ let config = {
   // any same or shared config
   // between environments can be here
   entry: './app/assets/scripts/App.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './app/index.html',
+    }),
+  ],
   module: {
     rules: [
       cssConfig,
@@ -84,10 +91,10 @@ if (currentTask == 'build') {
       },
     },
   };
-  config.plugins = [
+  config.plugins.push(
     new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({ filename: 'styles.[chunkhash].css' }),
-  ];
+    new MiniCssExtractPlugin({ filename: 'styles.[chunkhash].css' })
+  );
 }
 
 let deleteMeLater = {};

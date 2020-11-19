2686bd9f54829142fcf816180f98cd3a4500eccb
Ryan Johnson <ryanleejwebdev@gmail.com>
2020-11-19 13:15:59 -0600

    Make our JS backwards compatible with babel.

--- webpack.config.js
+++ webpack.config.js
@@ -89,6 +89,16 @@ if (currentTask == 'dev') {
 
 if (currentTask == 'build') {
   // code specific to build
+  config.module.rules.push({
+    test: /\.js$/,
+    exclude: /(node_modules)/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+      },
+    },
+  });
   cssConfig.use.unshift(MiniCssExtractPlugin.loader);
   postcssPlugins.push(require('cssnano'));
   config.output = {
